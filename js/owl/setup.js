ecd7706b1a0ffd652ab6f4b1866519deb17d0e34
Elson Manzalli <elson.manzalli@hotmail.com>
2021-11-10 21:40:40 -0300

    projeto Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 5,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
